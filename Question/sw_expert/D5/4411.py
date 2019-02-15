6ae9bc9d3f56ec886a0d73225fa440f525d01df6
unknown <ehdtlr9376@naver.com>
2019-02-15 14:41:02 +0900

    4411

---
+++ Question/sw_expert/D5/4411.py
@@ -0,0 +1,14 @@
+T = int(input())
+
+for i in range(T):
+    n,k = map(int, input().split())
+    k = k + 1
+    a = (k)%2
+    x = 3
+    while x <= n :
+        y = k % x
+        a = (y + a) % x
+        x += 1
+    a = a + 1
+    print(f"#{i+1} {a}")
+
